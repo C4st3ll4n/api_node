7828600d146b88eae607b3a66b53c9e8e5acdcf5
p_souza <psouza@braspag.com.br>
2021-07-24 20:13:12 -0300

    test: ensure bcrypt-adapter returns a hash on sucess

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,11 +1,17 @@
 import {BcryptAdapter} from "./bcrypt-adapter";
 import bcrypt from 'bcrypt'
 
+jest.mock('bcrypt', ()=>({
+    async hash (): Promise<String>{
+        return Promise.resolve("hash")
+    }
+}))
+
 interface SutTypes {
     sut: BcryptAdapter
 }
 
-const makeSut = (salt:number): SutTypes => {
+const makeSut = (salt:number = 12): SutTypes => {
     const sut = new BcryptAdapter(salt)
     return {sut}
 }
@@ -16,5 +22,13 @@ describe("BcrypterAdapter", () => {
         const encryptSpy = jest.spyOn(bcrypt, 'hash')
         await sut.encrypt("valid_password")
         expect(encryptSpy).toHaveBeenCalledWith("valid_password", salt)
+    })
+
+    test("ensure return hash on success", async ()=>{
+        const salt = 12
+        const {sut} = makeSut(salt)
+        const hash = await sut.encrypt('valid_password')
+        expect(hash).toBe('hash')
+
     })
 })
