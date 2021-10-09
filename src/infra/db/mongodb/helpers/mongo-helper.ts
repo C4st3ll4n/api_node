027a439c151973d1e4cc75621c7f1a463ea06a6b
p_souza <psouza@braspag.com.br>
2021-10-09 17:23:28 -0300

    chore: add mongo helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,13 @@
+import {MongoClient} from "mongodb";
+
+export const MongoHelper = {
+    client: null as MongoClient,
+
+    async connect(url: String):Promise<void>{
+        this.client = await MongoClient.connect(process.env.MONGO_URL, {})
+    },
+
+    async disconnect():Promise<void>{
+        await this.client.close();
+    }
+}
