6e857f4b4903da8eb053e4efc86f62623cd37e00
p_souza <psouza@braspag.com.br>
2021-10-11 09:48:23 -0300

    feat: mongo repository implementation

---
+++ src/infra/db/mongodb/account_repository/account.ts
@@ -0,0 +1,18 @@
+import {AddAccountRepository} from "../../../../data/protocols/add-account-repository";
+import {AddAccountModel} from "../../../../domain/usecases/add-account";
+import {AccountModel} from "../../../../domain/models/account";
+import {MongoHelper} from "../helpers/mongo-helper";
+
+export class AccountMongoRepository implements AddAccountRepository {
+
+    async add(account: AddAccountModel): Promise<AccountModel> {
+        const accountCollection = MongoHelper.getCollection("accounts")
+        const result = await accountCollection.insertOne(account)
+
+        const createdAccount = Object.assign({}, await accountCollection.findOne({_id: result.insertedId}),
+            {id: result.insertedId.toHexString()}) as AccountModel
+
+        return Promise.resolve(createdAccount);
+    }
+
+}
