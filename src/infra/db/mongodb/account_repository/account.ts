048659d47605d8f37343bd58659260d47ca43ccb
p_souza <psouza@braspag.com.br>
2022-01-24 10:59:36 -0300

    refactor: reconnect when mongo is down

--- src/infra/db/mongodb/account_repository/account.ts
+++ src/infra/db/mongodb/account_repository/account.ts
@@ -6,7 +6,7 @@ import {MongoHelper} from "../helpers/mongo-helper";
 export class AccountMongoRepository implements AddAccountRepository {
 
     async add(account: AddAccountModel): Promise<AccountModel> {
-        const accountCollection = MongoHelper.getCollection("accounts")
+        const accountCollection = await MongoHelper.getCollection("accounts")
         const result = await accountCollection.insertOne(account)
         const createdAccount = MongoHelper.map(await accountCollection.findOne({_id: result.insertedId}), result.insertedId)
         return Promise.resolve(createdAccount);
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,16 +2,22 @@ import {Collection, MongoClient} from "mongodb";
 
 export const MongoHelper = {
     client: null as MongoClient,
+    uri: null as String,
 
     async connect(url: string): Promise<void> {
+        this.uri = url
         this.client = await MongoClient.connect(url)
     },
 
     async disconnect(): Promise<void> {
         await this.client.close();
+        this.client = null
     },
 
-    getCollection(name: String): Collection {
+    async getCollection(name: String): Promise<Collection> {
+        if (!this.client || !this.client.isConnected) {
+            await this.connect(this.uri)
+        }
         return this.client.db().collection(name);
     },
 
